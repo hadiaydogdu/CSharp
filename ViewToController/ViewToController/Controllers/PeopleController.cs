3d48e4986267f13ef23046dd5e910ac09c4617b0
hadiaydogdu <h_aydogdu35@hotmail.com>
2020-12-17 00:21:31 +0300

    Added a project that shows how to pass a parameter from View to Controller

---
+++ ViewToController/ViewToController/Controllers/PeopleController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using ViewToController.Models;
+
+namespace ViewToController.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private PersonDBContext db = new PersonDBContext();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            return View(db.PersonsDB.ToList());
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.PersonsDB.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PersonsDB.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        public ActionResult Check()
+        {
+             return View(db.PersonsDB.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Check(string check_string)
+        {
+            List <Person> person = db.PersonsDB.ToList();
+            string result = "not exists";
+            foreach (Person p in person)
+            {
+                if (p.Name == check_string)
+                {
+                    result = "exists";
+                    break;
+                }
+            }
+            
+            ViewBag.Message = result;
+            return View(db.PersonsDB.ToList());
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.PersonsDB.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.PersonsDB.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Person person = db.PersonsDB.Find(id);
+            db.PersonsDB.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ViewToController/ViewToController/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace ViewToController.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class PersonDBContext : DbContext
+    {
+        public DbSet<Person> PersonsDB { get; set; }
+    }
+}
