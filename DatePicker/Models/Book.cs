9c56c52ed6dd56b11be293fc28ad3c82c18814e4
hadiaydogdu <h_aydogdu35@hotmail.com>
2020-12-17 15:23:03 +0300

    Added a project that shows how to use a datepicker as an input in C# MVC project

---
+++ DatePicker/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity; //Install EntityFramework by using NuGet packages
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BookingDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReturnDate { get; set; }
+    }
+
+
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> BookDB { get; set; }
+    }
+}
